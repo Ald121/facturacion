3cf0802e626dd2070687da64e2e3bbed9df0a4d7
Ald121 <fallsouls@hotmail.com>
2017-02-15 17:41:23 -0500

    reportes
    reportes

--- app/scripts/controllers/reportes_.js
+++ app/scripts/controllers/reportes_.js
@@ -8,7 +8,7 @@
  * Controller of the facturacionApp
  */
 var app =angular.module('facturacionApp');
-  app.controller('ReportesCtrl', function ($scope,$rootScope,Proformas_Services,Reportes_Services,$window,$mdDialog) {
+  app.controller('ReportesCtrl', function ($scope,$rootScope,Proformas_Services,Reportes_Services,$window,$mdDialog,LxNotificationService,$localStorage ) {
 		
 		console.log('reportes');
 //Productos Mas Vendidos
@@ -26,7 +26,7 @@ Reportes_Services.Productos().Mas_vendidos().send({},succes_mas_vendidos).$promi
 			if (error.status==500) {
 				LxNotificationService.error('Ha ocurrido un error ');
 			}
-			$LocalStorage.reset();
+			$localStorage.reset();
 
 		});;
 
@@ -38,25 +38,49 @@ $scope.series_x_mes=['Nro. Ventas'];
 function succes_x_mes(result){
   $scope.labels_x_mes = result.labels;
   $scope.data_x_mes = result.sumas;
+
+// angular-chart.js converts hex colors to objects that look like the objects in $scope.colors below. However, if we look angular-chart.js (not the minified file), it is missing getColor() on line 239 of angular-chart.js for rgba colors. As a result, rgba values submitted as "rgba(number, number, number, number)" are not converted into objects like the hex colors are. Because of this, the rest of the functions fail to run properly, and you get a weird chart.
+var chart = new Chart(angular.element(document.querySelector('#line')), {
+    type : 'line',
+    data : {
+        labels : $scope.labels_x_mes,
+        datasets : [{
+                label : 'Nro ventas',
+                data : $scope.data_x_mes,
+                backgroundColor : 'rgba(0, 53, 138, 0.7)',
+                borderColor : 'rgba(125, 216, 236, 1)',
+                borderWidth : 1
+            }
+        ]
+    },
+    options : {
+        responsive : true,
+        scales : {
+            yAxes : [{
+                    ticks : {
+                        beginAtZero : true
+                    }
+                }
+            ]
+        }
+    }
+});
+
 }
 Reportes_Services.Facturacion().ventas_x_mes().send({},succes_x_mes).$promise.then(function(){},function(error){
 
 			if (error.status==401) {
 				LxNotificationService.error('Su sesión ha caducado');
+				$localStorage.reset();
 			}
 
 			if (error.status==500) {
 				LxNotificationService.error('Ha ocurrido un error ');
 			}
-			$LocalStorage.reset();
+			
 
 		});;
 
-
-// $scope.meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"];
-// $scope.data_ventas = [
-//     [0, 59, 80, 81, 56, 55, 40,100]
-//   ];
 
 
 	  	});
